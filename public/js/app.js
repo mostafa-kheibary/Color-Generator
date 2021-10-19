456f6b6c5b821ef7f8558b3eebdf7adfa45745fe
mostafa kheibary <mostafamnbvcxz5@gmail.com>
2021-10-19 11:10:21 +0330

    add condition to when generated color is dark , all content goes light

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,9 @@
 var cont = document.getElementById('container');
-var text = document.getElementById('Color')
+var content = document.getElementById('content');
+var text = document.getElementById('Color');
+var butt = document.getElementById('butt');
+var Button = document.getElementById('Button');
+var HeadText = document.getElementById('text');
 function sum() {
     var x = Math.random()*256;
     var x = Math.round(x);
@@ -13,5 +17,21 @@ function sum() {
     var color= "rgb"+"("+x+","+y+","+z+")";
     cont.style.backgroundColor=color;
     text.innerHTML = color;
+    if(x<=153&&y<=153&&z<=153){
+        content.style.color="white";
+        HeadText.style.color="white";
+        butt.style.color="white";
+        Button.style.color="white";
+        butt.style.border="1px solid white";
+        Button.style.border="1px solid white";
+    }
+    else{
+        content.style.color="black";
+        HeadText.style.color="black";
+        butt.style.color="black";
+        butt.style.border="1px solid black";
+        Button.style.border="1px solid black";
+        Button.style.color="black";
+    }
 }
 
